63a9b3f6f7c1c72736c8c1bf9deaaac349fe7d7d
cleavon <cleavon.design.com>
2024-01-13 03:15:22 +0100

    updated

---
+++ bmi.py
@@ -0,0 +1,13 @@
+print("welcome to the BMI calculator")
+name = input("whats your name?")
+weight = float (input("whats your weight in kg?"))
+height = float(input("whats your height in m?"))
+bmi = weight/(height**2) 
+print( name +  "Your BMI is " + str(bmi) ) 
+
+if bmi < 18:
+  print("you are underweight")
+elif bmi < 25:
+    print("Normal weight")
+else:
+    print("Overweight")
