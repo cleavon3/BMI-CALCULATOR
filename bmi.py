621f3e042c8dc55d949f344c3a23185fd491f914
cleavon <cleavon.design.com>
2024-01-13 19:12:34 +0100

    updated

--- bmi.py
+++ bmi.py
@@ -5,7 +5,7 @@ height = float(input("whats your height in m?"))
 bmi = weight/(height**2) 
 print( name +  "Your BMI is " + str(bmi) ) 
 
-if bmi < 18:
+if bmi < 18.5:
   print("you are underweight")
 elif bmi < 25:
     print("Normal weight")
